74c2657eecf121cd51e4e29c5d3da0cba388aab0
Digital-Cookie <150790245+Digital-Cookie@users.noreply.github.com>
2025-03-12 22:36:29 +0900

    Add files via upload

---
+++ 필수 단계 1/필수 과정 1/문제 1/main.py
@@ -0,0 +1,22 @@
+print('Hello Mars')
+
+try:
+    with open('mission_computer_main.log', 'r') as file:
+        log_file = file.readlines()
+except:
+    print('Error in reading file.')
+else:
+    log = [i.strip() for i in log_file[1:]]
+    log.sort(key=lambda k: k.strip().split(',')[0], reverse=True)
+
+    print(log_file[0].strip())
+    for i in log:
+        print(i)
+
+    log_from = int(input('사고의 원인 시작: '))
+    log_to = int(input('사고의 원인 끝: '))
+    
+    with open('log_analysis.md', 'w') as md_file:
+        md_file.write(f'{log_file[0]}')
+        for i in log[log_from - 1:log_to]:
+            md_file.write(f'{i}\n')
